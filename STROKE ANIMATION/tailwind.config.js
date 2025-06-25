f3da5e4935b456f371c1a5546cbb125c4e9d2f9d
Ranuj Chaudhary <ranujchoudhary@gmail.com>
2025-06-25 11:39:03 +0530

    animation css architechture

---
+++ STROKE ANIMATION/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      keyframes: {
+        'border-spin': {
+          '100%': {
+            transform: 'rotate(-360deg)',
+          },
+        },
+      },
+      animation: {
+        'border-spin': 'border-spin 7s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+};
